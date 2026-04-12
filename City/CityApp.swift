106a2c107a9356553facf01bf734a35b901aeea4
James Junaidi <james.junaidi@gmail.com>
2026-04-12 13:20:31 -0700

    Initial Commit

---
+++ City/CityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityApp.swift
+//  City
+//
+//  Created by James Junaidi on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
